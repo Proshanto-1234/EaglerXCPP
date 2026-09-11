717ab2148a2ab3ec789613646cd3dc221754f32b
Proshanto-1234 <proshantobrahmmachary@outlook.com>
2026-09-11 11:46:54 +0600

    Initialize global variables in MACRODefinitions.cpp
    Added global variables and structures for engine management.

--- MACRODefinitions.cpp
+++ MACRODefinitions.cpp
@@ -1,1 +1,12 @@
+// Additional generic definition file
 #include "DEFINITIONS.hpp"
+
+std::atomic<bool> ENGINE = true; // Global engine variable 
+unsigned long long listenSock = INVALID_SOCKET;
+void* hIOCP = NULL;
+inline std::unordered_map<unsigned long long, PLAYER_SESSION> ActiveSessions;
+CRITICAL_SECTION SessionLock;
+std::vector<GAME_ENTITY> GlobalEntities;
+thread_local ThreadArena WorkerArena;
+GPUChunkSerializer g_gpuSerializer;
+Console currentWindow;
